b1d98e884492a9630bdb8079f07ca38461a22f40
Paweł Rzepecki <pawel.rzepecki21@gmail.com>
2024-08-23 19:34:35 +0200

    added container with database

---
+++ app/app/calc.py
@@ -0,0 +1,14 @@
+"""
+Calculator functions 
+"""
+
+
+def add(x, y):
+    """Adds x to y"""
+    return x + y
+
+
+def substract(x, y):
+    """Substracts y from x"""
+
+    return x - y
---
+++ app/app/tests.py
@@ -0,0 +1,22 @@
+"""
+Sample tests
+"""
+
+from django.test import SimpleTestCase
+
+from app import calc
+
+class CalcTests(SimpleTestCase):
+    """Test the calc module/"""
+    def test_add(self):
+        """Test adding numbers together"""
+        result = calc.add(5, 6)
+
+        self.assertEqual(result, 11)
+
+    
+    def test_substract(self):
+        """Test substract numbers."""
+        res = calc.substract(5, 3)
+
+        self.assertEqual(res, 2)
