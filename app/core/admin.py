2ee35527673695c05e6ce39b1a9bdd4e93f71274
Paweł Rzepecki <pawel.rzepecki21@gmail.com>
2024-09-14 18:26:12 +0200

    added tags api and tags extension for recipe api

--- app/core/admin.py
+++ app/core/admin.py
@@ -4,7 +4,7 @@ Django admin customization.
 
 from django.contrib import admin  # noqa
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-from core.models import User, Recipe
+from core.models import User, Recipe, Tag
 from django.utils.translation import gettext_lazy as _
 
 
@@ -54,3 +54,4 @@ class UserAdmin(BaseUserAdmin):
 
 admin.site.register(User, UserAdmin)
 admin.site.register(Recipe)
+admin.site.register(Tag)
